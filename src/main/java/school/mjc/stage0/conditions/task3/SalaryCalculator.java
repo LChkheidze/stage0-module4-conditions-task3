14bce8571b101021bc6562a1b9e3d89525b57e93
LChkheidze <levanchx@gmail.com>
2023-05-10 00:05:31 +0400

    Multiple conditions

--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        if(salary < 0) {
+            System.out.println("wrong input!");
+        }
+
+        if(salary > 0 && salary <=10_000) {
+            System.out.printf("%.1f\n", salary * 0.85);
+        } else if(salary > 10_000 && salary <= 20_000) {
+            System.out.printf("%.1f\n", salary * 0.82);
+        } else {
+            System.out.printf("%.1f\n", salary * 0.8);
+        }
     }
 }
